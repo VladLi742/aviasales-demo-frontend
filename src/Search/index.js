3505b744d7d771ed70e2fd2646ad0c0d42df75b8
VladLi742 <vladislav.likhachev742@gmail.com>
2018-02-21 11:50:46 +0300

    Added unique title for search-page

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import Header from "./Header";
 import Main from "./Main";
@@ -6,6 +7,11 @@ import Main from "./Main";
 export default function() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Поиск билетов</title>
+      </Helmet>
+
       <Header />
       <Main />
     </div>
